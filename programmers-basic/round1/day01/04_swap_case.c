d8395afe1ac3347777e4f535e86eb421816efc0e
MinCheol An <mincheol701@gmail.com>
2026-01-02 10:59:03 +0900

    feat: round1 day01 완료 (문제 5개 및 회고 작성)

---
+++ programmers-basic/round1/day01/04_swap_case.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define LEN_INPUT 20
+
+int main(void) {
+    char s1[LEN_INPUT];
+    scanf("%s", s1);
+    
+    for(int i = 0; s1[i] != '\0'; i++) {
+        if(s1[i] >= 65 && s1[i] <= 90)
+            s1[i] = s1[i] + 32;
+        else if(s1[i] >= 97 && s1[i] <= 122)
+            s1[i] = s1[i] - 32;
+    }
+    
+    printf("%s", s1);
+    
+    return 0;
+}
